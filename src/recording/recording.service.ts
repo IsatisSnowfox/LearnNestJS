909aa13d4813b4f6293e86df749e6739044b5de8
Félix Gason <isatis.snowfox@gmail.com>
2024-04-04 14:54:43 -0400

    Add start of recording

--- src/recording/recording.service.ts
+++ src/recording/recording.service.ts
@@ -6,9 +6,15 @@ import axios from 'axios';
 @Injectable({ scope: Scope.REQUEST })
 export class RecordingService {
     appId: string;
+    azureStorageName;
+    azureStorageAccessKey;
+    azureStorageContainerName;
 
     constructor(config: ConfigService) {
         this.appId = config.get<string>('agora.appId');
+        this.azureStorageName = config.get<string>('AzureStorageAccount.storageName');
+        this.azureStorageAccessKey = config.get<string>('AzureStorageAccount.accessKey');
+        this.azureStorageContainerName = config.get<string>('AzureStorageAccount.containerName');
     }
 
     async acquire(cname: string, uid: number): Promise<any> {
@@ -20,9 +26,33 @@ export class RecordingService {
         return response.data;
     }
 
-    async start(resourceId: string, mode: string): Promise<any> {
+    async start(resourceId: string, mode: string, channelName: string, uid: number): Promise<any> {
         const url = `https://api.agora.io/v1/apps/${this.appId}/cloud_recording/resourceid/${resourceId}/mode/${mode}/start`;
-        await axios.post(url, {  });
+        let reponse = await axios.post(url, {
+            "cname": channelName,
+            "uid": uid,
+            "recordingConfig": {
+                "streamMode": "standard",
+                "transcodingConfig": {
+                    "width": 1920,
+                    "height": 1080,
+                    "bitrate": 2000,
+                    "mixedVideoLayout": 1
+                }
+            },
+            "recordingFileConfig": {
+                "avFileType": ["hls", "mp4"]
+            },
+            "storageConfig": {
+                "vendor": 5, // Microsoft Azure,
+                "accessKey": this.azureStorageName,
+                "secretKey": this.azureStorageAccessKey,
+                "bucket": this.azureStorageContainerName,
+                "fileNamePrefix": [channelName]
+            }
+        });
+
+        return reponse.data;
     }
 
     async stop(resourceId: string, sid: string, mode: string): Promise<any> {
